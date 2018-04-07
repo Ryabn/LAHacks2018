01a9aa60083f956a74df198bd882ac900f05f6c1
Ryan Yang <javathon@gmail.com>
2018-04-07 00:28:41 -0700

    changed test server to production

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var emptyTime = '--:--:--';
 
 //cors
 app.all('/', function(req, res, next) {
-    //res.header('Access-Control-Allow-Origin', 'https://ryabn.github.io');
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://ryabn.github.io');
+    //res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     next();
